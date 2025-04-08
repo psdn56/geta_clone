a0d2f27a8dd9ee201afe13f744e3e525fc1cb49d
psdn56 <psdn56@gmail.com>
2025-04-08 21:00:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,6 +77,19 @@ if (checkoutBtn) {
     alert('Proceeding to checkout...');
     // Integrate payment/redirect here
   });
+  function generateOrderID(userMobile) {
+  const now = new Date();
+  const yy = String(now.getFullYear()).slice(-2);
+  const mm = String(now.getMonth() + 1).padStart(2, '0');
+  const dd = String(now.getDate()).padStart(2, '0');
+  const hh = String(now.getHours()).padStart(2, '0');
+  const min = String(now.getMinutes()).padStart(2, '0');
+  const ss = String(now.getSeconds()).padStart(2, '0');
+
+  const last4Digits = userMobile.slice(-4);
+
+  return `OID${yy}${mm}${dd}${last4Digits}${hh}${min}${ss}`;
+  }
   // script.js
 
 const slider = document.getElementById("heroSlider");
