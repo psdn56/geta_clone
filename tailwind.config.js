15afe3733b8c792b0ffbfa0b0565cc1ac1faf8a6
psdn56 <psdn56@gmail.com>
2025-04-15 14:04:40 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./path/to/your/html/**/*.html",  // Adjust this path as needed
+    "./path/to/your/js/**/*.js",
+    // Add more file paths if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
